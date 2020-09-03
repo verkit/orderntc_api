3bb886d652b75f4907df7fcf517fa4f1965e8982
verkit <verdybangkit@gmail.com>
2020-09-03 11:19:18 +0700

    update config database

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,22 @@
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PASSWORD: "root",
+//     DB: "orderntc",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
+
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "root",
-    DB: "orderntc",
+    HOST: "control.castor.idgx.net",
+    USER: "u13051ezu_sj",
+    PASSWORD: "special",
+    DB: "u13051ezu_sj",
     dialect: "postgres",
     pool: {
         max: 5,
